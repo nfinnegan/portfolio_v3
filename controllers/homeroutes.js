c0ff62639bae8b8bdd85c69d452cf12e57082931
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-19 23:11:27 -0600

    project 1 outline added

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
-    // Pass serialized data and session flag into template
     res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/projects", async (req, res) => {
+  try {
+    res.render("projects");
   } catch (err) {
     res.status(500).json(err);
   }
